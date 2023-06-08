33d681c2d44f238ea38e7779e989b936f0476009
Vivian Vu <vhlananhjr@gmail.com>
2023-06-08 16:03:35 -0400

    * Validated user input (existing title + empty) * Showed toast message * Hided long task title

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { v4 as uuidV4 } from 'uuid';
 const list = document.querySelector<HTMLUListElement>('#task-list');
 const taskInput = document.querySelector<HTMLInputElement>('#task-title');
 const form = document.querySelector<HTMLFormElement>('#form');
+const toastList = document.querySelector<HTMLDivElement>('#toast-list');
 
 type Task = {
   id: string;
@@ -15,11 +16,19 @@ let taskList: Task[] = loadTasks();
 
 form?.addEventListener('submit', (e) => {
   e.preventDefault();
-  if (taskInput?.value === '' || taskInput?.value === undefined) return;
+  if (taskInput?.value === '' || taskInput?.value === undefined) {
+    showToast('Invalid task title!');
+    return;
+  }
+
+  if (validateNewTask(taskInput.value.trim())) {
+    showToast('Task already exists!');
+    return;
+  }
 
   const newTask: Task = {
     id: uuidV4(),
-    title: taskInput.value,
+    title: taskInput.value.trim(),
     complete: false,
     createdAt: new Date(),
   };
@@ -81,6 +90,32 @@ function renderTask() {
   taskList.forEach((task) => {
     addNewTask(task);
   });
+}
+
+function validateNewTask(taskTitle: string): boolean {
+  return taskList.some(
+    (task) => taskTitle.toLowerCase() === task.title.toLowerCase(),
+  );
+}
+
+function showToast(message: string) {
+  const newToast = document.createElement('div');
+  newToast.classList.add('toast');
+  newToast.innerHTML = `
+  <i class="bx bx-error"></i>
+        <span>${message}</span>
+        <span class="count-down"></span>`;
+
+  toastList?.append(newToast);
+
+  let id1 = setTimeout(() => {
+    newToast.style.animation = 'hide 2s ease-in-out forwards';
+    clearTimeout(id1);
+  }, 4000);
+  let id2 = setTimeout(() => {
+    newToast.remove();
+    clearTimeout(id2);
+  }, 5000);
 }
 
 renderTask();
